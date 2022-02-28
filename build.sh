57431bdf7ec58cbbe809ff43da705e2df1d1807b
pedrozc90 <pedrozc90@gmail.com>
2022-02-28 20:31:20 -0300

    update build.sh

--- build.sh
+++ build.sh
@@ -2,4 +2,27 @@
 
 sh normalize.sh
 
-docker build --no-cache --file ./Dockerfile --tag pedrozc90/rabbitmq .
+docker build --no-cache \
+    --file ./Dockerfile \
+    --tag pedrozc90/rabbitmq:test \
+    .
+    
+docker run -it \
+    --rm \
+    --name rabbitmq \
+    --hostname rabbitmq \
+    --publish 1883:1883 \
+    --publish 5671:5671 \
+    --publish 5672:5672 \
+    --publish 15672:15672 \
+    --publish 15674:15674 \
+    --publish 15675:15675 \
+    --publish 15692:15692 \
+    --publish 25672:25672 \
+    --publish 61613:61613 \
+    --env RABBITMQ_DEFAULT_USER=admin \
+    --env RABBITMQ_DEFAULT_PASS=admin \
+    --env RABBITMQ_DEFAULT_VHOST=admin \
+    --log-opt max-size=10m \
+    --log-opt max-file=5 \
+    pedrozc90/rabbitmq:test sh
